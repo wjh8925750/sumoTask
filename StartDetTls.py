fe0a8234f6d8cf7114211cefb8812521d22d6048
wjh8925750 <793017100@qq.com>
2020-09-28 19:20:36 +0800

    second commit

--- StartDetTls.py
+++ StartDetTls.py
@@ -1,29 +1,25 @@
 from dettlssumo import SUMO
 
 import config
-
-
-
-
 class Main:
     def __init__(self):
         self.scenario_path = './scenario/'
         self.init_param()
         self.init_sumo()          #配置sumo启动文件
 
-
     def init_param(self):
         from configparser import ConfigParser
         cfg = ConfigParser()
         cfg.read(self.scenario_path + 'param.ini', encoding='utf-8')
         config.sumocfg = cfg.get('sumoconfig', 'sumocfg')
-        config.radius = cfg.get('sumoconfig', 'radius')
+        config.radius = cfg.getint('sumoconfig', 'radius')
         config.cinductionloopid = cfg.get('sumoconfig', 'cinductionloopid')
         print(config.sumocfg)
+        print(type(config.radius))
 
     def init_sumo(self):
         self.sumo = SUMO(config.sumocfg)   #sumoconfig中应传入将要打开的.sumocfg文件的路径
-        #self.sumo.subInductionloop(config.cinductionloopid, config.radius)
+        self.sumo.subInductionloop(config.cinductionloopid, config.radius)
 
     def start_sumo(self):
         self.sumo.simulation()
---
+++ udp.py
@@ -0,0 +1,16 @@
+import socket
+
+
+class UDP_Server:
+    def __init__(self, port, queue):
+        self.port = port
+        self.udpSerSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udpSerSock.bind(('', self.port))
+        self.queue = queue
+    def run(self):
+        BUFSIZE = 32 * 1024
+        while True:
+            data, addr = self.udpSerSock.recvfrom(BUFSIZE)
+            # print("Receive from %s:%s" % addr)
+            # print("Msg: ", data)
+            self.queue.put(data)
